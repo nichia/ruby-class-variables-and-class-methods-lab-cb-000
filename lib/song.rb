cf66b282be793a2d4b14aa6e554b0a2f7c82c33f
Ni Chia <nichia@gmail.com>
2017-11-05 00:48:46 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << name
+    @@genres << name
+  end
+
+  def count
+    @@count
+  end
+
+  def artists
+  end
+
+  def genres
+
+  end
+end
