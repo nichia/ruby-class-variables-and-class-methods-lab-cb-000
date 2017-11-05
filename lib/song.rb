a7cfc3ebbb31549be860508c5e3b7ac74a499260
Ni Chia <nichia@gmail.com>
2017-11-05 01:12:19 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    result = {}
+    @@genres.each { |genre|
+      if result[genre] == nil
+        result[genre]= 1
+      else
+        result[genre] += 1
+      end
+    }
+    result
+  end
+
+  def self.artist_count
+    result = {}
+    @@artists.each { |artist|
+        if result[artist] == nil
+          result[artist] = 1
+        else
+          result[artist] += 1
+        end
+    }
+    result
+  end
+
+end
